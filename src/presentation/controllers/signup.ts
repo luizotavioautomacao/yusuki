45ad51f525c37639f0a3ab98e6036b31c3fc783a
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-10-13 02:50:54 -0300

    refactor: move ServerError to a http helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,7 @@
 import { IAddAccount } from "../../domain/usecases/add-account";
 import { InvalidParamError } from "../errors/invalid-param-error";
 import { MissingParamError } from "../errors/missing-param-error";
-import { ServerError } from "../errors/server-param-error";
-import { badRequest } from "../helpers/http-helper";
+import { badRequest, serverError } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/email-validator";
 import { HttpRequest, HttpResponse } from "../protocols/http";
@@ -33,15 +32,12 @@ export class SignUpController implements Controller {
                 return badRequest(new InvalidParamError('passwordConfirmation'))
             }
             const account = this.addAccount.add({ name, email, password })
-            return{
-                statusCode:200,
+            return {
+                statusCode: 200,
                 body: account
             }
         } catch (error) {
-            return {
-                statusCode: 500,
-                body: new ServerError()
-            }
+            return serverError()
         }
     }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,8 +1,16 @@
+import { ServerError } from "../errors/server-param-error"
 import { HttpResponse } from "../protocols/http"
 
 export const badRequest = (error: Error): HttpResponse => {
     return {
         body: error,
         statusCode: 400
+    }
+}
+
+export const serverError = (): HttpResponse => {
+    return {
+        statusCode: 500,
+        body: new ServerError()
     }
 }
