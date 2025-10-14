12ed030e9c33ae9a579ce690d5ab9debc9fc99c9
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-10-13 23:03:12 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/ultis/email-validator-adapter.spec.ts
@@ -0,0 +1,11 @@
+import { EmailValidatorAdapter } from "./email-validator-adapter"
+
+describe('', () => {
+
+    test('Should return false if validator returns false', async () => {
+        const sut = new EmailValidatorAdapter()
+        const isValid = sut.isValid('invalid_email@mail.com')
+        expect(isValid).toBe(false)
+    })
+
+})
