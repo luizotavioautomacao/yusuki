90e34f8347b6ff8b68ed984f77dbe375ec37c8a5
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-10-13 23:25:08 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/ultis/email-validator-adapter.spec.ts
+++ src/ultis/email-validator-adapter.spec.ts
@@ -1,11 +1,34 @@
 import { EmailValidatorAdapter } from "./email-validator-adapter"
+import validator from 'validator'
+
+const email = "email@mail.com";
+
+jest.mock('validator', () => ({
+    isEmail(): boolean {
+        return true
+    }
+}))
 
 describe('', () => {
 
     test('Should return false if validator returns false', async () => {
         const sut = new EmailValidatorAdapter()
-        const isValid = sut.isValid('invalid_email@mail.com')
+        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+        const isValid = sut.isValid(email)
         expect(isValid).toBe(false)
+    })
+
+    test('Should return true if validator returns true', async () => {
+        const sut = new EmailValidatorAdapter()
+        const isValid = sut.isValid(email)
+        expect(isValid).toBe(true)
+    })
+
+    test('Should call validator with correct email', async () => {
+        const sut = new EmailValidatorAdapter()
+        const isValidSpy = jest.spyOn(validator, 'isEmail')
+        sut.isValid(email)
+        expect(isValidSpy).toHaveBeenCalledWith(email)
     })
 
 })
