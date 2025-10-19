505f3c20cdf30a5123528eb5ff01c5dee644578b
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-10-19 01:24:59 -0300

    test: ensure BrcyptAdapter return hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import { BcryptAdapter } from "./bcrypt-adapter";
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', () => ({
+    async hash(): Promise<string> {
+        return new Promise((resolve) => resolve('hash'))
+    }
+}))
+
 describe('Brcypt Adapter', () => {
 
     test('Should call bcrypt with coorect values', async () => {
@@ -9,6 +15,13 @@ describe('Brcypt Adapter', () => {
         const hash = jest.spyOn(bcrypt, 'hash')
         await sut.encrypt('any_value')
         expect(hash).toHaveBeenLastCalledWith('any_value', salt)
+    })
+
+    test('Should return a has on success', async () => {
+        const salt = 12
+        const sut = new BcryptAdapter(salt);
+        const hash = await sut.encrypt('any_value')
+        expect(hash).toBe('hash')
     })
 
 })
