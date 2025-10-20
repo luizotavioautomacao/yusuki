8053f588e21fd931f8d2d7e4d7bf6119f72990fc
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-10-19 22:48:06 -0300

    test: ensure request return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from "../configs/app"
+
+describe('Content-Type Middleware', () => {
+
+    test('Should return content-type as json', async () => {
+        app.get('/test_content_type', (req, res) => {
+            res.send()
+        })
+
+        await request(app)
+            .get('/test_content_type')
+            .expect('content-type', /json/)
+    })
+
+})
