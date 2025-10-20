bd082ae5f607de39b24990bac5b4d80ba47b0548
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-10-19 22:52:19 -0300

    feat: ensure requests return xml content-type when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -13,4 +13,15 @@ describe('Content-Type Middleware', () => {
             .expect('content-type', /json/)
     })
 
+    test('Should return xml content-type when forced', async () => {
+        app.get('/test_content_type_xml', (req, res) => {
+            res.type('xml')
+            res.send()
+        })
+
+        await request(app)
+            .get('/test_content_type_xml')
+            .expect('content-type', /xml/)
+    })
+
 })
