0e1ae6b4df613bb6888f0bd6c8d85142b6b7f2a9
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-10-19 22:33:49 -0300

    feat: ensure cors is enabled

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import app from "../configs/app"
 
-describe('Body Parser Middleware ', () => {
+describe('Body Parser Middleware', () => {
 
     test('Should parse body as json', async () => {
         app.post('/test_body_parser', (req, res) => {
---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest'
+import app from "../configs/app"
+
+describe('CORS Middleware ', () => {
+
+    test('Should enabled CORS', async () => {
+        app.post('/test_cors', (req, res) => {
+            res.send()
+        })
+
+        await request(app)
+            .get('/test_cors')
+            .expect('access-control-allow-origin', '*')
+            .expect('access-control-allow-methods', '*')
+            .expect('access-control-allow-headers', '*')
+    })
+
+})
